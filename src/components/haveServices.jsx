0d6a06ce4e22a2d83f919e5ac052895615943041
Ignas Koncius <i.koncius@gmail.com>
2021-07-19 08:47:34 +0300

    empty components

---
+++ src/components/haveServices.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HaveServices extends Component {
+  render() {
+    return <h2>HaveServices component</h2>;
+  }
+}
+
+export default HaveServices;
---
+++ src/components/mobilePlan.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MobilePlan extends Component {
+  render() {
+    return <h2>Mobile Plan component</h2>;
+  }
+}
+
+export default MobilePlan;
