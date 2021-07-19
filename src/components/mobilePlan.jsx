85c6f09f8a87cc381d8a3b3542c83937b199d762
Ignas Koncius <i.koncius@gmail.com>
2021-07-19 10:26:41 +0300

    mobile plan html part

--- src/components/mobilePlan.jsx
+++ src/components/mobilePlan.jsx
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 
 class MobilePlan extends Component {
   render() {
-    return <h2>Mobile Plan component</h2>;
+    return (
+      <div className="mobile-plan">
+        <h6 className="plan__header">Mobilus START 1</h6>
+        <h2 className="plan__dataAllow">1 GB</h2>
+        <small className="plan__dataEU">Iš jų 1 GB ES/EEE</small>
+        <p className="plan__min-sms">Neribotos MIN ir SMS</p>
+        <div className="plan__features">Sarasas</div>
+        <div className="plan__bottom">
+          <div className="bottom__price-part">
+            <h3>9,00 eur/men</h3>
+            <small>24 mėn. sutartis</small>
+          </div>
+          <button className="plan__cta">Domina</button>
+        </div>
+      </div>
+    );
   }
 }
 
