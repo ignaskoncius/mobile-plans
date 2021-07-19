085c0b3049d8eb107a3c6b1416bece86740006ab
Ignas Koncius <i.koncius@gmail.com>
2021-07-19 11:54:49 +0300

    mobile plan card css done

--- src/components/mobilePlan.jsx
+++ src/components/mobilePlan.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ListImage from '../static/check.png';
 
 class MobilePlan extends Component {
   render() {
@@ -8,7 +9,20 @@ class MobilePlan extends Component {
         <h2 className="plan__dataAllow">1 GB</h2>
         <small className="plan__dataEU">Iš jų 1 GB ES/EEE</small>
         <p className="plan__min-sms">Neribotos MIN ir SMS</p>
-        <div className="plan__features">Sarasas</div>
+        <div className="plan__features">
+          <div className="hr"></div>
+          <ul className="features__list">
+            <li>
+              <img className="features__check" src={ListImage} alt="check" />
+              <span>M. parašas (6 mėn.)</span>
+            </li>
+            <li>
+              <img className="features__check" src={ListImage} alt="check" />
+              <span>Įrangos draudimas (3 mėn.)</span>
+            </li>
+          </ul>
+          <div className="hr"></div>
+        </div>
         <div className="plan__bottom">
           <div className="bottom__price-part">
             <h3 className="bottom__price">
