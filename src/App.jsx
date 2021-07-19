acdb1599d74ed2bc74fd3e615cde926162bd0a3f
Ignas Koncius <i.koncius@gmail.com>
2021-07-19 08:47:20 +0300

    change from functional component to class

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Mobile plans</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Mobiliojo ryšio planai</h1>
+        <p>Visos Lietuvoje turimo plano naudos galioja Baltijos ir Skandinavijos šalyse.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
